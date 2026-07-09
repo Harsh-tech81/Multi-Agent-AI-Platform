7146bbfeffe47bf63b2913143c77bad6e7b54f88
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-07-09 17:13:36 +0530

    connected to Mongo

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import connectDb from "./config/db.js";
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Auth Service is running" });
@@ -12,4 +13,5 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Auth Service is running on port ${PORT}`);
+  connectDb();
 });
