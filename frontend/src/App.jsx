79e0471b534ded4f2afb282b3d8f24458b66540d
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-07-10 22:45:09 +0530

    make google auth login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,10 @@
-import { auth, googleProvider } from "../utils/firebase";
-import {signInWithPopup} from "firebase/auth";
-
+import Home from "./pages/Home";
 
 function App() {
-  const googleLogin = async () => {
-    const data = await signInWithPopup(auth, googleProvider);
-    console.log(data);
-  };
-
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={googleLogin}>
-        Continue with Google
-      </button>
-    </div>
+    <>
+      <Home />
+    </>
   );
 }
 
