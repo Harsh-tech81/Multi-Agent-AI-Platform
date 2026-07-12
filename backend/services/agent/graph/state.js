892d63604236a290df5d47ce747dc5e4fa706141
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-07-12 16:15:12 +0530

    creating custom state for Agent to access all LangGraph Nodes and store prompt and AI_Response

---
+++ backend/services/agent/graph/state.js
@@ -0,0 +1,7 @@
+import { Annotation } from "@langchain/langgraph";
+
+// creating a custom state for the agent to store the prompt and the AI response in LangGraph. This state can be used to keep track of the conversation context and any relevant information that the agent needs to remember during its interactions with users.
+export const agentState=Annotation.Root({
+     prompt: Annotation(),
+  aiResponse: Annotation(),
+})
