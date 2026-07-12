0cd3f75e59bda06fddd72c20498f267e9659414b
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-07-12 14:52:08 +0530

    Creating Agent Service

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -3,20 +3,23 @@ import dotenv from "dotenv";
 import proxy from "express-http-proxy";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import {proxyWithHeader} from "./utils/proxyWithHeader.js";
+import { proxyWithHeader } from "./utils/proxyWithHeader.js";
 dotenv.config();
 import protect from "./middleware/auth.middleware.js";
 import { getCurrUser } from "./controllers/user.controller.js";
 const app = express();
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
 app.use(cookieParser());
 const PORT = process.env.PORT || 8000;
 
-app.use("/api/auth",proxy( process.env.AUTH_SERVICE_URL));
-app.use("/api/chat",proxyWithHeader( process.env.CHAT_SERVICE_URL));
+app.use("/api/auth", proxy(process.env.AUTH_SERVICE_URL));
+app.use("/api/chat",protect, proxyWithHeader(process.env.CHAT_SERVICE_URL));
+app.use("/api/agent", protect, proxy(process.env.AGENT_SERVICE_URL));
 app.get("/api/me", protect, getCurrUser);
 app.get("/", (req, res) => {
   res.status(200).json({ message: "Gateway is running" });
--- backend/services/agent/index.js
+++ backend/services/agent/index.js
@@ -1,20 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDb from "./config/db.js";
-import router from "./routes/chat.routes.js";
+
 dotenv.config();
-const PORT = process.env.PORT || 8002;
+const PORT = process.env.PORT || 8003;
 
 const app = express();
 app.use(express.json());
-app.use("/",router);
-
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Chat Service is running" });
+  res.status(200).json({ message: "Agent Service is running" });
 });
 
 app.listen(PORT, () => {
-  console.log(`Chat Service is running on port ${PORT}`);
+  console.log(`Agent Service is running on port ${PORT}`);
   connectDb();
 });
