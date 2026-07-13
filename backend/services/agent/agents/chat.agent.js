dadcf40c3a0062bc3465818b95e0e3834991201e
Harsh Kumar <[harshkr.221104@gmail.com]>
2026-07-13 16:16:35 +0530

    creating chatAgent for simple chat with LLM

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,3 +1,21 @@
-export const chatAgent= async(params) => {
+import { getModel } from "../config/llmModels.js";
 
+export const chatAgent= async(state) => {
+const llm =await getModel("chat");
+const Systemprompt="You are CortexAI, an Intelligent AI assistant.";
+const response = await llm.invoke([
+    {
+        "role": "system",
+        "content": Systemprompt
+    },
+    {
+        role: "human",
+        content: state.prompt
+    }
+]);
+
+return {
+    ...state,
+    aiResponse: response.content
 }
+};
